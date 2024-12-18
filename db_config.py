c1f0cab412bf8a63ed328c042851f1596a37964e
Abo Danny <nawrizibrahim@gmail.com>
2024-12-17 19:32:46 -0500

    adding database configration file

---
+++ db_config.py
@@ -0,0 +1,11 @@
+# Database connection
+import mysql.connector
+
+def get_connection():
+    return mysql.connector.connect(
+       host="bus-system-db.mysql.database.azure.com",
+        user="nawriz",
+        password="Dina@1989",
+        database="bus_system_DB",
+        ssl_ca="C:\Users\Abo Danny\Desktop\DigiCertGlobalRootCA.crt.pem"
+    )
